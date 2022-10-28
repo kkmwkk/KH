f48ed983c8258d847771d7c787776ba73e5c134f
miw woo <mlolw2@naver.com>
2022-10-28 11:42:33 +0900

    servlet commit

---
+++ KH/src/main/java/test_1/HelloServlet.java
@@ -0,0 +1,32 @@
+package test_1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/HelloTest")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		
+		//HTML을 생성할 수 있는 PrintWriter 객체를 저장
+		PrintWriter out = response.getWriter();
+		out.println("<h2>Hello Servlet 전민우 !!!!<h2>");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
